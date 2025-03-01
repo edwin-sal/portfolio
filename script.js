c9aea67eeb5d1e331cfacc5ea5cc13c6b097b041
Edwin Bañago <ej.banago00601@student.tsu.edu.ph>
2025-03-01 10:21:32 +0800

    Hide the welcome content when door is clicked.

--- script.js
+++ script.js
@@ -27,8 +27,20 @@ const showEye = () => {
 	eyes.style.display = 'block';
 };
 
+// Hides the div containing the welcome content
+const hideWelcomeContainer = () => {
+	const welcomeContainer = document.getElementById('welcome-container');
+	welcomeContainer.style.display = 'none';
+};
+
 // Add event listener for the door
 document.getElementById('door-button').addEventListener('click', () => {
-	console.log('clicked');
+	// Show the eye for 3.6 seconds and disable the pointer events at that duration so the user cant click the door multiple times
 	showEye();
+	document.body.style.pointerEvents = 'none';
+	
+	setTimeout(() => {
+		hideWelcomeContainer();
+		document.body.style.pointerEvents = 'auto';
+	}, 3600)
 });
